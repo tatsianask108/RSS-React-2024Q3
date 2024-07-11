71d9f494605165c00daf5e3e4c2a41336cf8d33e
tatsianask108 <tt.laurie@gmail.com>
2024-07-11 17:29:12 +0300

    refactor: move buttons styles to component folder

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react';
 import { IButton } from './types';
+import styles from './Button.module.css';
 
 const Button: FC<IButton> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+  return (
+    <button className={styles.btn} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
